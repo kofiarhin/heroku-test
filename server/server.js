8ddd5ce915267126be859f7b5b2efe3c0c814469
kofi arhin <kofiarhin69@gmail.com>
2025-08-19 11:39:08 +0100

    testing pipeline

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res, next) => {
-  return res.json({ message: "hello world" });
+  return res.json({ message: "this is to test continuos integration" });
 });
 
 const port = process.env.PORT || 5000;
